08b57f3fadaaa95e7bcc43fc55d5f623bf7b716c
Ojiemeke-Michael <ojiemekemichaelolusegun@gmail.com>
2023-03-07 12:21:18 +0100

    made some series of files to solve the problem of creating a function that locates a character in a string.

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -6,4 +6,34 @@
  * Prototype: char *_strchr(char *s, char c);
  * Returns a pointer to the first occurrence of the character c
  * in the string s, or NULL if the character is not found
- * FYI: The standard library provides a similar function: strchr"
+ * FYI: The standard library provides a similar function: "strchr"
+ *
+ * @s: the string to be searched.
+ * @c: the character used for searching.
+ *
+ * Return: a pointer to the character 'c', if not, return "NULL"
+ */
+char *_strchr(char *s, char c)
+{
+	char * search_ptr; /* this is the pointer to be return when 'c' is found */
+	int count; /* this helps us go through each of s characters */
+
+	if (s != NULL)
+	{
+		count = 0;
+		while (*(s + count) != '\0')
+		{
+			if (*(s + count) == c)
+			{
+				search_ptr = (s + count);
+				break;
+			}
+
+			count++;
+		}
+
+		return (search_ptr);
+	}
+	else
+		return (NULL);
+}
