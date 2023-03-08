96f2533d1e8fe32ae68d79bf2e765e5ace4dc2ac
Ojiemeke-Michael <ojiemekemichaelolusegun@gmail.com>
2023-03-08 12:14:52 +0100

    made a bunch of files that solves the problem of creating a function in C that prints a string in reverse using recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - this is a program that prints a string in reverse.
+ *
+ * Prototype: void _print_rev_recursion(char *s);
+ *
+ * @s: the string which is to be printed in reverse
+ *
+ * Return: void.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (s != NULL)
+	{
+		if (*s == '\0')
+			return;
+		else
+		{
+			_print_rev_recursion(s + 1);
+			_putchar(*s);
+		}
+	}
+}
